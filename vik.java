cd850a4b1a0eee39664e3eeaed89439b203a8843
vikhyath <vikhyath459@gmail.com>
2026-04-29 15:16:12 +0530

    int-cmt

--- vik.java
+++ vik.java
@@ -2,5 +2,7 @@ public class vik{
 	public static void main(String[] args){
 		int a=10, int b=2;
 		system.out.println("Addition is:+(a+b));
+		system.out.println("Difference is:+(a-b));
+		system.out.println("Product is:+(a*b));
 	}
 }
