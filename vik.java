8e6cfb8f115965a5dd33d9ec05b32cb520f27543
vikhyath <vikhyath459@gmail.com>
2026-04-29 15:32:48 +0530

    Revert "div"
    This reverts commit 067872414bfe63c55c199de5d9169abf7fb053a2.

--- vik.java
+++ vik.java
@@ -4,6 +4,5 @@ public class vik{
 		system.out.println("Addition is:+(a+b));
 		system.out.println("Difference is:+(a-b));
 		system.out.println("Product is:+(a*b));
-		system.out.println("Division is:+(a/b))
 	}
 }
